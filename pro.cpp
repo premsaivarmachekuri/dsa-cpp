273ab4e92eba894f4321b51b0f205e48d2120a28
Premsai Varma <premsaivarmacoding@gmail.com>
2023-12-29 23:23:27 +0530

    added the prep setup

---
+++ pro.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int, long, long long, float, double
+    // string getline
+    // char
+    string s = "Apple pro";
+    cout << s[8];
+
+    return 0;
+}
