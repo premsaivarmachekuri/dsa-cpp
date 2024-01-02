dda4ad579cd281045c36bd280894775a95d4a8ef
Premsai Varma <premsaivarmacoding@gmail.com>
2024-01-02 23:18:49 +0530

    updated the files

--- pro.cpp
+++ pro.cpp
@@ -5,8 +5,14 @@ int main(){
     // int, long, long long, float, double
     // string getline
     // char
+    int n=5;
     string s = "Apple pro";
     cout << s[8];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            // Code 
+        }
+    }
 
     return 0;
 }
